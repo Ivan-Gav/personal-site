03f5982003976df2af57985f9fd5860c03da9789
Ivan Gavrilin <ivan.gavrilin@inbox.ru>
2026-02-20 17:03:01 +0300

    content: edit last updated

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -83,7 +83,7 @@ export default function Footer() {
         </div>
         <div
           className={s.copyright}
-        >{`©2024 - ${currentYear} ${t('IG')} | ${t('LAST_UPDATED')} - 22.07.2025`}</div>
+        >{`©2024 - ${currentYear} ${t('IG')} | ${t('LAST_UPDATED')} - 20.02.2026`}</div>
       </footer>
       <div ref={ref} className={s.bottom} />
     </>
