e5c006ece86411bcb921de01d64310df779804ba
Ivan Gavrilin <ivan.gavrilin@inbox.ru>
2026-08-28 15:15:46 +0400

    perf: preload avatar from html to remove lcp discovery delay

--- src/components/Photo/Photo.tsx
+++ src/components/Photo/Photo.tsx
@@ -1,9 +1,10 @@
 import { useState } from 'react';
 import cn from 'classnames';
 import s from './Photo.module.css';
-import photoAvif from 'src/assets/pics/avatar_v2.avif';
-import photoWebp from 'src/assets/pics/avatar_v2.webp';
-import photo from 'src/assets/pics/avatar_v2.png';
+
+const AVIF = '/pics/avatar_v2.avif';
+const WEBP = '/pics/avatar_v2.webp';
+const PNG = '/pics/avatar_v2.png';
 
 export default function Photo() {
   const [loaded, setLoaded] = useState(false);
@@ -11,11 +12,11 @@ export default function Photo() {
   return (
     <div className={s.photo_frame}>
       <picture className={s.picture}>
-        <source srcSet={photoAvif} type="image/avif" />
-        <source srcSet={photoWebp} type="image/webp" />
+        <source srcSet={AVIF} type="image/avif" />
+        <source srcSet={WEBP} type="image/webp" />
         <img
           className={cn(s.photo, loaded && s.loaded)}
-          src={photo}
+          src={PNG}
           alt="hi!"
           fetchPriority="high"
           onLoad={() => setLoaded(true)}
