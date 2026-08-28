590a1e7989edb75c494906a97786e1a034d5acfd
Ivan Gavrilin <ivan.gavrilin@inbox.ru>
2026-08-28 14:55:12 +0400

    perf: serve avatar as avif/webp with png fallback

--- src/components/Photo/Photo.tsx
+++ src/components/Photo/Photo.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import cn from 'classnames';
 import s from './Photo.module.css';
+import photoAvif from 'src/assets/pics/avatar_v2.avif';
+import photoWebp from 'src/assets/pics/avatar_v2.webp';
 import photo from 'src/assets/pics/avatar_v2.png';
 
 export default function Photo() {
@@ -8,12 +10,17 @@ export default function Photo() {
 
   return (
     <div className={s.photo_frame}>
-      <img
-        className={cn(s.photo, loaded && s.loaded)}
-        src={photo}
-        alt="hi!"
-        onLoad={() => setLoaded(true)}
-      />
+      <picture className={s.picture}>
+        <source srcSet={photoAvif} type="image/avif" />
+        <source srcSet={photoWebp} type="image/webp" />
+        <img
+          className={cn(s.photo, loaded && s.loaded)}
+          src={photo}
+          alt="hi!"
+          fetchPriority="high"
+          onLoad={() => setLoaded(true)}
+        />
+      </picture>
     </div>
   );
 }
